70d9312f58ac7f5599252eba2099632066541f2f
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-23 23:31:12 +0400

    added Example 4 Recursion

---
+++ Example4_recursion/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+// Вычисление факториала
+
+double Factorial(int n)
+{
+    if (n == 1) return 1;              // Если факториал = 1 - возвращаем 1
+    else return n * Factorial(n - 1);   // если нет то умножаем тек. значение (n) и умножить на предыдущие число 
+}
+//Console.WriteLine(Factorial(3));     // выводим факториал 3 (1*2*3 = 6)
+
+for (int i = 1; i < 40; i++)
+    {
+        Console.WriteLine($"{i}! = {Factorial(i)} ");    // Stack overflow ((
+    }
