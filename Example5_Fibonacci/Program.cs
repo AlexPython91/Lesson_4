0b0766aa2515d7237e3c4bc156461cca238fd4e7
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-23 23:31:43 +0400

    added Example 5 Fibonacci

---
+++ Example5_Fibonacci/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+// Числа Фибоначчи
+
+// для первого числа f(1) = 1
+// для второго f(2) = 1
+// для всех след. f(n) = f(n-1) + f(n-2)
+
+double Fibonacci(int n)   // тип метода int, название Fibonacci, возвр.арг число (n), можно double
+{
+    if (n == 1 || n == 2) return 1;            // если n == 1, или n == 2, то возвращаем n
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+for (int i = 1; i < 50; i++)        // Цикл от 1 до 10
+{
+    Console.WriteLine($"({i}) = {Fibonacci(i)} ");
+}
