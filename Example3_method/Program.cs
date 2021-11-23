9e32fce6c6be07ccc96c7306fe838c4d84468bef
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-23 23:30:16 +0400

    added Example 3 Method

---
+++ Example3_method/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+// Печать массива: Метод тип void/ название PrintArray/ аргумент двуменрый массив
+
+void PrintArray(int[,] matr)   // Меняем название на matr
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Метод заполнения массива тип void/ название PrintArray/ аргумент двуменрый массив
+
+void FillArray(int[,] matr)   // Меняем название на matr
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+int[,] matrix = new int[3, 6];
+PrintArray(matrix);     // Вызов метода печати
+Console.WriteLine();    // Пробел между массивами 
+FillArray(matrix);       // Вызов метода заполнения массива
+PrintArray(matrix);     // Печать заполненного массива
